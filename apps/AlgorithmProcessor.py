f8d41179ec85d0509e1d496eac13a8d44e3a89df
bilalimg <bilalimamogluu@gmail.com>
2021-09-19 18:01:33 +0300

    add docs and remove unnecessary parameter

--- apps/AlgorithmProcessor.py
+++ apps/AlgorithmProcessor.py
@@ -7,20 +7,34 @@ from sklearn.naive_bayes import BernoulliNB
 
 
 class AlgorithmProcessor:
+    """
+    Base class Algorithms that will run.
+    """
+
     def __init__(self,
-                 file_string: str,
                  pickle_path: str,
                  model_type: str):
+        """
+        Base class of AlgorithmProcessor.
 
-        self.file_string: str = file_string
+        @param pickle_path: Jobs that will run on cluster.
+        @param model_type: All necessary tags for resources.
+        """
         self.pickle_path: str = pickle_path
         self.model_type: str = model_type
 
     def run(self):
+        """
+        Init function of AlgorithmProcessor, which initiates actions to be displayed in the interface in Streamlit.
+        """
         self.add_train_option()
         self.predict_input()
 
     def add_train_option(self):
+        """
+        If exits, loads classifier and vectorizer for NLP model. It also adds a button that triggers the new model
+        training. If the user clicks, the new model is trained, and when completed, it replaces the old model.
+        """
         if st.sidebar.button("Click to train a new model"):
             self.train_model()
         else:
@@ -30,7 +44,10 @@ class AlgorithmProcessor:
                 st.error(f"Error occurred while getting models, please train a new model.")
 
     def train_model(self):
-        train_doc, train_labels = self.load_data()
+        """
+        Trains the model for the selected algorithm. Saves classifier and vectorizer as pickle.
+        """
+        train_doc, train_labels = self.load_training_data()
         self.vectorizer = CountVectorizer()
         vector = self.vectorizer.fit_transform(train_doc)
 
@@ -43,17 +60,31 @@ class AlgorithmProcessor:
 
         return
 
-    def load_data(self):
+    def load_training_data(self):
+        """
+        Loads train data for training.
+
+        @return: Train data and Train labels.
+        """
         return
 
     def get_classifier(self):
+        """
+        Returns the required classifier for the selected model type.
+
+        @return: Classifier.
+        """
         if self.model_type == "RandomForestClassifier":
             return RandomForestClassifier()
         else:
             return BernoulliNB()
 
-    def predict_input(self):
+    def inference_the_input(self):
+        """
+        Provides the necessary input scheme to the user to enter the text and inferences this input.
+        """
         input = st.text_input("Enter The Sentence", "Enter The Text Here...")
+
         if st.button('Predict The Result'):
             result = self.classifier.predict(self.vectorizer.transform([input]))[0]
             self.print_result(result)
@@ -61,14 +92,26 @@ class AlgorithmProcessor:
             st.write("Press the above button..")
 
     def print_result(self, result):
+        """
+        Prints the inference result.
+
+        @param result: Inference result.
+        """
         st.success(result)
 
     def dump_pickle(self):
-        pickle.dump(self.vectorizer, open(f"{self.pickle_path}{self.file_string}_vectorizer.pickle", "wb"))
-        pickle.dump(self.classifier, open(f"{self.pickle_path}{self.file_string}_finalized_model.sav", 'wb'))
+        """
+        Saves vectorizer and classifier pickles.
+        """
+        pickle.dump(self.vectorizer, open(self.pickle_path + "vectorizer.pickle", "wb"))
+        pickle.dump(self.classifier, open(self.pickle_path + "finalized_model.sav", 'wb'))
 
     def load_pickle(self):
-        vectorizer = pickle.load(open(f"{self.pickle_path}{self.file_string}_vectorizer.pickle", 'rb'))
-        classifier = pickle.load(open(f"{self.pickle_path}{self.file_string}_finalized_model.sav", 'rb'))
+        """
+        Loads vectorizer and classifier pickles if they exit.
 
+        @return: Vectorizer and Classifier.
+        """
+        vectorizer = pickle.load(open(self.pickle_path + "vectorizer.pickle", 'rb'))
+        classifier = pickle.load(open(self.pickle_path + "finalized_model.sav", 'rb'))
         return vectorizer, classifier
